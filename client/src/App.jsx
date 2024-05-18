52ed4ab33d2c6fcc6a9c3c20ae42f3160b32de00
Oleg <belinskiy21@gmail.com>
2024-05-18 15:06:34 +0100

    Added Vite React client app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BreedForm from './features/breeds/breedForm';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Dog breeds</h1>
+      <BreedForm />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/features/breeds/breedForm.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { API_URL } from "../../constants";
+
+function BreedForm() {
+    return <>BreedForm</>;
+}
+export default BreedForm;
