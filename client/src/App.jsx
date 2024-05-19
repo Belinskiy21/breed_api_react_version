63915b47f80a04ae75875bf5c81e3a656e812a3f
Oleg <belinskiy21@gmail.com>
2024-05-19 23:25:24 +0100

    Redirect rails root to the public index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import BreedForm from './features/breeds/breedForm';
 import AppRoutes from './components/appRoutes';
 
 function App() {
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_17_205917) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dogs", force: :cascade do |t|
     t.string "breed"
     t.datetime "created_at", null: false
